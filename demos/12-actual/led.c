e6e62e8a42236d5d572c3f2c7ea23ba567dad1ed
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-07-20 11:11:49 -0600

    Update led.c

--- demos/12-actual/led.c
+++ demos/12-actual/led.c
@@ -5,16 +5,17 @@
 
 char switch_state_changed, switch_state_down;
 
-void led_init()
+/*void led_init()
 {
   P1DIR |= LEDS;		// bits attached to leds are output
   switch_state_changed = 1;
   led_update();
-}
+} */
 
+/*
 void led_update(){
   if (switch_state_changed) {
-    char ledFlags = 0; /* by default, no LEDs on */
+    char ledFlags = 0; //by default, no LEDs on 
 
     ledFlags |= switch_state_down ? LED_GREEN : 0;
     ledFlags |= switch_state_down ? 0 : LED_RED;
@@ -23,5 +24,5 @@ void led_update(){
     P1OUT |= ledFlags;         // set bits for on leds
   }
   switch_state_changed = 0;
-}
+} */
 
