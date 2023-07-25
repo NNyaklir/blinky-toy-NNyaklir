e71c47f2e829accbad21b0a4b059415438a9b344
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-07-25 17:16:30 -0600

    major update 2

--- demos/12-actual/blinkyToy.c
+++ demos/12-actual/blinkyToy.c
@@ -32,66 +32,68 @@ int main(void) {
     led_off(LED_RED);
 
     while (1) {
-        int b1STATE = (P2IN & SW1) ? 1 : 0;
-        int b2STATE = (P2IN & SW2) ? 1 : 0;
-        int b3STATE = (P2IN & SW3) ? 1 : 0;
-        int b4STATE = (P2IN & SW4) ? 1 : 0;
+        
 
-        switch (currentState) {
-            case STATE_OFF:
-                if (b1STATE) {
-                    currentState = LED_GREEN_ON;
-                    led_on(LED_GREEN);
-                    greenState = !greenState;
-                    buzzer_play_for_duration(5);
-                } else if (b2STATE) {
-                    currentState = LED_RED_ON;
-                    led_on(LED_RED);
-                    redState = !redState;
-                    buzzer_play_for_duration(5);
-                }
-                break;
-            case LED_GREEN_ON:
-                if (b3STATE) {
-                    currentState = STATE_OFF;
-                    led_off(LED_GREEN);
-                    buzzer_play_for_duration(5);
-                } else if (b2STATE) {
-                    currentState = BOTH_LED_ON;
-                    led_on(LED_RED);
-                    redState = !redState;
-                    buzzer_play_for_duration(5);
-                }
-                break;
-            case LED_RED_ON:
-                if (b1STATE) {
-                    currentState = BOTH_LED_ON;
-                    led_on(LED_GREEN);
-                    greenState = !greenState;
-                    buzzer_play_for_duration(5);
-                }
-                if (b4STATE) {
-                    currentState = STATE_OFF;
-                    led_off(LED_RED);
-                    buzzer_play_for_duration(5);
-                }
-                break;
-            case BOTH_LED_ON:
-                if (b3STATE) {
-                    currentState = LED_RED_ON;
-                    led_off(LED_GREEN);
-                    buzzer_play_for_duration(5);
-                } else if (b4STATE) {
-                    currentState = LED_GREEN_ON;
-                    led_off(LED_RED);
-                    buzzer_play_for_duration(5);
-                }
-                break;
+        int prevB1State = 1; // Previous state of button 1
+        int prevB2State = 1; // Previous state of button 2
+        int prevB3State = 1; // Previous state of button 3
+        int prevB4State = 1; // Previous state of button 4
+
+        while (1) {
+        // Read the current button states
+        int b1State = (P2IN & SW1) ? 1 : 0;
+        int b2State = (P2IN & SW2) ? 1 : 0;
+        int b3State = (P2IN & SW3) ? 1 : 0;
+        int b4State = (P2IN & SW4) ? 1 : 0;
+
+        // Check for button 1 press
+        if (b1State == 0 && prevB1State == 1) {
+            currentState = LED_GREEN_ON;
+            led_on(LED_GREEN);
+            greenState = !greenState;
+            buzzer_play_for_duration(5);
         }
+
+        // Check for button 2 press
+        if (b2State == 0 && prevB2State == 1) {
+            currentState = LED_RED_ON;
+            led_on(LED_RED);
+            redState = !redState;
+            buzzer_play_for_duration(5);
+        }
+
+        // Check for button 3 press
+        if (b3State == 0 && prevB3State == 1) {
+            if (currentState == LED_GREEN_ON) {
+                currentState = STATE_OFF;
+                led_off(LED_GREEN);
+            } else if (currentState == LED_RED_ON || currentState == BOTH_LED_ON) {
+                currentState = LED_RED_ON;
+                led_off(LED_GREEN);
+            }
+            buzzer_play_for_duration(5);
+        }
+
+        // Check for button 4 press
+        if (b4State == 0 && prevB4State == 1) {
+            if (currentState == LED_RED_ON) {
+                currentState = STATE_OFF;
+                led_off(LED_RED);
+            } else if (currentState == LED_GREEN_ON || currentState == BOTH_LED_ON) {
+                currentState = LED_GREEN_ON;
+                led_off(LED_RED);
+            }
+            buzzer_play_for_duration(5);
+        }
+
+        // Update the previous button states
+        prevB1State = b1State;
+        prevB2State = b2State;
+        prevB3State = b3State;
+        prevB4State = b4State;
 
         // Add a delay to avoid button bounce issues
         __delay_cycles(200000);
+        }
     }
-
-    return 0;
 }
